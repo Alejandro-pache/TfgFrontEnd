cd4a9b151855d7724d964c72a066bbef909986dc
Alejandro-pache <chichonduro33@gmail.com>
2026-02-10 16:40:40 +0100

    Me he equivocado y el anterior commit era para enviar las

--- app/src/main/java/com/example/tfgfrontend/ui/start/StartFragment.kt
+++ app/src/main/java/com/example/tfgfrontend/ui/start/StartFragment.kt
@@ -8,7 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.tfgfrontend.databinding.FragmentStartBinding
 import com.example.tfgfrontend.R
-//Este es el fragment en le que comienza la aplicacion.
+//Este es el fragment en le que comienza la aplicacion
 class StartFragment : Fragment() {
     private var _binding: FragmentStartBinding? = null
     private val binding get() = _binding!!
